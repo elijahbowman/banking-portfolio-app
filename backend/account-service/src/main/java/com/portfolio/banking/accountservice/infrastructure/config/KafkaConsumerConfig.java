bdeb9f345c7a105c5c0fcd1e17a2058047c37281
elijahbowman <bowmanel2@vcu.edu>
2025-12-05 18:34:11 -0500

    Adds Prometheus, Grafana, and Loki

---
+++ backend/account-service/src/main/java/com/portfolio/banking/accountservice/infrastructure/config/KafkaConsumerConfig.java
@@ -0,0 +1,23 @@
+package com.portfolio.banking.accountservice.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.getContainerProperties().setObservationEnabled(true);
+
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+}
---
+++ backend/banking-service/src/main/java/com/portfolio/banking/bankingservice/infrastructure/config/KafkaProducerConfig.java
@@ -0,0 +1,21 @@
+package com.portfolio.banking.bankingservice.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(
+            ProducerFactory<String, Object> producerFactory) {
+
+        KafkaTemplate<String, Object> template = new KafkaTemplate<>(producerFactory);
+
+        template.setObservationEnabled(true);
+
+        return template;
+    }
+}
